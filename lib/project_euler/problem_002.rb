a74b532b9f45f1a6ff7f9c111563aaa776c6cd3e
Victor Koronen <koronen@kth.se>
2011-07-30 21:21:52 +0200

    Add solution for problem #002

--- lib/project_euler/problem_002.rb
+++ lib/project_euler/problem_002.rb
@@ -5,7 +5,13 @@ require File.dirname(__FILE__) + '/problem_base'
 module ProjectEuler
   class Problem002 < ProjectEuler::ProblemBase
     def answer
-      0
+      fibs = [1, 2]
+
+      while fibs.last < 4000000 do
+        fibs << fibs[-2] + fibs[-1]
+      end
+
+      fibs.select(&:even?).inject(&:+)
     end
   end
 end
