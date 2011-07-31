f2e94fadee7ae1b3890756e4194819a80d91b368
Victor Koronen <koronen@kth.se>
2011-07-31 02:59:06 +0200

    Add chain_length spec for problem #014

--- spec/project_euler/problem_014_spec.rb
+++ spec/project_euler/problem_014_spec.rb
@@ -6,6 +6,12 @@ describe ProjectEuler::Problem014 do
     @instance = ProjectEuler::Problem014.new
   end
 
+  it "calculates chain length correctly" do
+    @instance.chain_length(1).should eq(0)
+    @instance.chain_length(2).should eq(1)
+    @instance.chain_length(3).should eq(7)
+  end
+
   it "gives the correct answer" do
     @instance.answer.should eq(837799)
   end
