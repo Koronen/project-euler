be17f636b698cc5e2f9168ae99f17404414fd9b3
Victor Koronen <koronen@kth.se>
2011-08-02 17:43:28 +0200

    Add execution time test to #001

--- spec/project_euler/problem_001_spec.rb
+++ spec/project_euler/problem_001_spec.rb
@@ -3,6 +3,8 @@ require 'problem_001'
 
 module ProjectEuler
   describe Problem001 do
+    it_is_solvable_in_time { Problem001.answer }
+
     it "gives the correct answer" do
       Problem001.answer.should eq(233168)
     end
