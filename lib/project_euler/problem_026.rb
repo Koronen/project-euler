6ffde2cd0ebb409794110bea5d682e323e355a74
Victor Koronen <koronen@kth.se>
2011-08-28 12:40:12 +0200

    Add #026 stub

---
+++ lib/project_euler/problem_026.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+$: << File.join(File.expand_path(File.dirname(__FILE__)))
+require 'problem_base'
+
+module ProjectEuler
+  class Problem026 < ProjectEuler::ProblemBase
+    class << self
+      def fraction_recurring_cycle(a, b)
+        fraction = a.to_f / b.to_f
+        cycle = fraction.to_s.scan(/(\d+)(?:\1)+\d*$/).flatten.first
+        cycle
+      end
+
+      def answer!
+        longest_cycle = 0
+        has_longest_cycle = 0
+
+        1.upto(999) do |n|
+          fraction = fraction_recurring_cycle(1, n)
+          next if fraction.nil?
+          if fraction.length > longest_cycle
+            longest_cycle = fraction.length
+            has_longest_cycle = n
+          end
+        end
+
+        has_longest_cycle
+      end
+    end
+  end
+end
+
+run_problem! if $0 == __FILE__
---
+++ spec/project_euler/problem_026_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'problem_026'
+
+module ProjectEuler
+  describe Problem026 do
+    it "calculates fraction_recurring_cycle correctly" do
+      pending("need to find a new way of extracting fractions")
+      Problem026.fraction_recurring_cycle(1, 2).should be_nil # no recurring cycle
+      Problem026.fraction_recurring_cycle(1, 3).should == "3"
+      Problem026.fraction_recurring_cycle(1, 4).should be_nil
+      Problem026.fraction_recurring_cycle(1, 5).should be_nil
+      Problem026.fraction_recurring_cycle(1, 6).should == "6"
+      Problem026.fraction_recurring_cycle(1, 7).should == "142857"
+      Problem026.fraction_recurring_cycle(1, 8).should be_nil
+      Problem026.fraction_recurring_cycle(1, 9).should == "1"
+      Problem026.fraction_recurring_cycle(1, 10).should be_nil
+    end
+
+    it "gives the correct answer" do
+      pending("need to find a new way of extracting fractions")
+      Problem026.answer.should eq(0)
+    end
+  end
+end
