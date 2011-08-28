1b6fdeaa30fcb40e4be1fc9add0a7045b8f092fb
Victor Koronen <koronen@kth.se>
2011-08-28 12:26:59 +0200

    Solves #028

---
+++ lib/project_euler/problem_028.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+$: << File.join(File.expand_path(File.dirname(__FILE__)))
+require 'problem_base'
+
+module ProjectEuler
+  class Problem028 < ProjectEuler::ProblemBase
+    def self.diagonal_sum(side_length)
+      if side_length <= 1
+        1
+      else
+        sum = self.diagonal_sum(side_length-2)
+        
+        high = side_length**2
+        sum += high
+        sum += high - (side_length-1)*1
+        sum += high - (side_length-1)*2
+        sum += high - (side_length-1)*3
+        
+        sum
+      end
+    end
+
+    def self.answer!
+      self.diagonal_sum(1001)
+    end
+  end
+end
+
+run_problem! if $0 == __FILE__
---
+++ spec/project_euler/problem_028_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'problem_028'
+
+module ProjectEuler
+  describe Problem028 do
+    it "calculates diagonal_sum correctly" do
+      Problem028.diagonal_sum(1).should eq(1)
+      Problem028.diagonal_sum(3).should eq(25)
+      Problem028.diagonal_sum(5).should eq(101)
+    end
+
+    it "gives the correct answer" do
+      Problem028.answer.should eq(669171001)
+    end
+  end
+end
