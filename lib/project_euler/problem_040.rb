e51fa763239d8f7acb6e682a16cec9771dd40a5b
Victor Koronen <koronen@kth.se>
2011-10-28 22:52:52 +0200

    Solves #040

---
+++ lib/project_euler/problem_040.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+$: << File.join(File.expand_path(File.dirname(__FILE__)))
+require 'problem_base'
+
+module ProjectEuler
+  class Problem040 < ProjectEuler::ProblemBase
+    class << self
+      # 0.12345678910111213141516...
+      def nth_fraction_digit(n)
+        return n if n < 10
+
+        digits_in_number = 1
+        digits_total = 9
+        while true
+          digits_in_number += 1
+          low = digits_total + 1
+          high = low + digits_in_number * (9 * (10 ** (digits_in_number-1))) - 1
+          if n >= low and n <= high
+            range_index = n - low
+            number_index = (range_index / digits_in_number.to_f).floor
+            digit_index = range_index % digits_in_number
+            number_containing_digit = 10 ** (digits_in_number - 1) + number_index
+            b = 10 ** (digits_in_number - digit_index - 1)
+            return (number_containing_digit / b) % 10
+          else
+            digits_total = high
+          end
+        end
+      end
+
+      def answer!
+        0.upto(6).map{|i| nth_fraction_digit(10 ** i) }.nproduct
+      end
+    end
+  end
+end
+
+run_problem! if $0 == __FILE__
---
+++ spec/project_euler/problem_040_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'problem_040'
+
+module ProjectEuler
+  describe Problem040 do
+    it_is_solvable_in_time { Problem040.answer }
+
+    it "calculates nth_fraction_digit correctly" do
+      Problem040.nth_fraction_digit(1).should eq(1)
+      Problem040.nth_fraction_digit(2).should eq(2)
+      Problem040.nth_fraction_digit(10).should eq(1) #10
+      Problem040.nth_fraction_digit(11).should eq(0)
+      Problem040.nth_fraction_digit(30).should eq(2) #20
+      Problem040.nth_fraction_digit(31).should eq(0)
+      Problem040.nth_fraction_digit(90).should eq(5) #50
+      Problem040.nth_fraction_digit(91).should eq(0)
+      Problem040.nth_fraction_digit(188).should eq(9) #99
+      Problem040.nth_fraction_digit(189).should eq(9)
+      Problem040.nth_fraction_digit(190).should eq(1) #100
+      Problem040.nth_fraction_digit(191).should eq(0)
+      Problem040.nth_fraction_digit(192).should eq(0)
+    end
+
+    it "gives the correct answer" do
+      Problem040.answer.should eq(210)
+    end
+  end
+end
