7475ffe3263ef4d374d45701f9513ea5f5391c48
Victor Koronen <koronen@kth.se>
2011-12-21 11:52:52 +0100

    Perform cosmetic changes to #030

--- lib/project_euler/problem_030.rb
+++ lib/project_euler/problem_030.rb
@@ -5,23 +5,25 @@ require 'problem_base'
 
 module ProjectEuler
   class Problem030 < ProjectEuler::ProblemBase
-    def self.digits_power_sum(n, power)
-      sum = 0
+    class << self
+      def digits_power_sum(n, power)
+        sum = 0
 
-      while n > 0
-        sum += (n % 10)**power
-        n /= 10
+        while n > 0
+          sum += (n % 10)**power
+          n /= 10
+        end
+
+        sum
       end
 
-      sum
-    end
+      def nth_power_digits_numbers(power)
+        11.upto(1_000_000).select{|n| n == self.digits_power_sum(n, power) }
+      end
 
-    def self.nth_power_digits_numbers(power)
-      2.upto(1_000_000).select{|n| n > 10 && n == self.digits_power_sum(n, power) }
-    end
-
-    def self.answer!
-      self.nth_power_digits_numbers(5).sum
+      def answer!
+        self.nth_power_digits_numbers(5).sum
+      end
     end
   end
 end
