4a55dc361c3a45b8d2aa216c8333e1139af96c8f
Victor Koronen <koronen@kth.se>
2011-12-21 11:48:30 +0100

    Don't fail fast per default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.filter_run_excluding :slow => true # tests that take more than 5 seconds
-
-  config.fail_fast = true
 end
 
 =begin
