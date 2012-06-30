c96c095e053c35ae22140b67f22073bdd8671aba
Victor Koronen <koronen@kth.se>
2012-06-30 15:45:22 +0200

    Skip unneeded sorting

--- lib/project_euler/problem_024.rb
+++ lib/project_euler/problem_024.rb
@@ -1,8 +1,7 @@
 module ProjectEuler
   class Problem024 < ProjectEuler::ProblemBase
     def self.answer!
-      (0..9).to_a.permutation(10).sort.each_with_index{|e,i| return e.join.to_i if i == 999_999 }
-      nil
+      (0..9).to_a.permutation(10).each_with_index{|p,i| return p.join.to_i if i == 999_999 }
     end
   end
 end
