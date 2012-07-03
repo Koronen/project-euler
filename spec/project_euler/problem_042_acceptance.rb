db1507d96433f1f087a5e2cdfeb31d6595a9b635
Victor Koronen <koronen@kth.se>
2012-07-03 23:57:38 +0200

    Solve #042

---
+++ spec/project_euler/problem_042_acceptance.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+require 'project_euler/problem_042'
+
+module ProjectEuler
+  describe Problem042 do
+    it 'gives the correct answer' do
+      Problem042.answer.must_equal 162
+    end
+  end
+end
---
+++ spec/project_euler/problem_042_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+require 'project_euler/problem_042'
+
+module ProjectEuler
+  describe Problem042 do
+    describe '#word_value' do
+      it 'works' do
+        Problem042.word_value('SKY').must_equal 55
+      end
+    end
+  end
+end
