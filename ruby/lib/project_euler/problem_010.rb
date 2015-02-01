5e48b05ff14119b214f8584280b38d36351728ed
Victor Koronen <koronen@kth.se>
2015-02-01 17:44:41 +0100

    Simplify #010 solution

--- ruby/lib/project_euler/problem_010.rb
+++ ruby/lib/project_euler/problem_010.rb
@@ -1,14 +1,7 @@
 module ProjectEuler
   class Problem010 < ProjectEuler::ProblemBase
     def self.answer!
-      sum = 0
-
-      Prime.each do |p|
-        break if p > 2_000_000
-        sum += p
-      end
-
-      sum
+      Prime.take_while { |p| p <= 2_000_000 }.inject(0, :+)
     end
   end
 end
