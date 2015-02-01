b19a75a6aa55ea21ed19434599dab2b7718414a5
Victor Koronen <koronen@kth.se>
2015-02-01 18:29:26 +0100

    Optimize #006 solution

--- ruby/lib/project_euler/problem_006.rb
+++ ruby/lib/project_euler/problem_006.rb
@@ -1,8 +1,8 @@
 module ProjectEuler
   class Problem006 < ProjectEuler::ProblemBase
     def self.answer!
-      square_of_sum = (1..100).inject(&:+) ** 2
-      sum_of_squares = (1..100).map{|x| x**2 }.inject(&:+)
+      square_of_sum = (100 * 100) * (101 * 101) / (2 * 2)
+      sum_of_squares = 100 * 101 * (2 * 100 + 1) / 6
       square_of_sum - sum_of_squares
     end
   end
