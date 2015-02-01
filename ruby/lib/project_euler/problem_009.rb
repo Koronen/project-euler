8afb117fdedddbe6a10f008ec431a6f46723d6c0
Victor Koronen <koronen@kth.se>
2015-02-01 17:57:51 +0100

    Optimize #009 solution

--- ruby/lib/project_euler/problem_009.rb
+++ ruby/lib/project_euler/problem_009.rb
@@ -1,18 +1,10 @@
 module ProjectEuler
   class Problem009 < ProjectEuler::ProblemBase
     def self.answer!
-      1.upto(1000) do |a|
-        (a+1).upto(1000) do |b|
-          c = Math.sqrt(a**2 + b**2).to_i
-
-          # not a triplet
-          next if c**2 != a**2 + b**2
-
-          # too large
-          break if a + b + c > 1000
-
-          # sought after triplet
-          return a*b*c if a + b + c == 1000
+      1.upto(333) do |a|
+        (a + 1).upto(500) do |b|
+          c = 1000 - a - b
+          return a * b * c if c * c == a * a + b * b
         end
       end
     end
